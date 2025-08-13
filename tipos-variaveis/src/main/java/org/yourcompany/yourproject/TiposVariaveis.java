73e9932f4f6629e1c3fcb884b7e5cbd004448633
nortoncunha <norton_nt22@outlook.com>
2025-08-13 14:23:51 -0300

    criando o projeto para exemplo de tipos variaveis

---
+++ tipos-variaveis/src/main/java/org/yourcompany/yourproject/TiposVariaveis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author nortoncunha
+ */
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
