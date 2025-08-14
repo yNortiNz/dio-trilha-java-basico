21f7b06369d666bdb4c07b18e659a21dc76bcc52
nortoncunha <norton_nt22@outlook.com>
2025-08-14 12:59:15 -0300

    Finalizando a aula sobre tipos variaveis

--- tipos-variaveis/src/main/java/org/yourcompany/yourproject/TiposVariaveis.java
+++ tipos-variaveis/src/main/java/org/yourcompany/yourproject/TiposVariaveis.java
@@ -11,6 +11,25 @@ package org.yourcompany.yourproject;
 public class TiposVariaveis {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+
+        String meuNome = "Norton Cunha";
+        
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //adicionado o cast como adaptação com consciencia de que se ocorrer algo inesperado estamos cientes, não é usado com frequencia.
+        
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14; //determinação de uma constante onde seu valor não pode ser alterado. importante ser em CAIXA ALTA.
+
     }
 }
