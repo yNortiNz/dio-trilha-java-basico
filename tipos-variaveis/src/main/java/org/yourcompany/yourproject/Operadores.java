75915e089e45daa59b3ea3a23d15c73f2343cae0
nortoncunha <norton_nt22@outlook.com>
2025-08-14 13:10:45 -0300

    Iniciando a aula sobre operadores

---
+++ tipos-variaveis/src/main/java/org/yourcompany/yourproject/Operadores.java
@@ -0,0 +1,8 @@
+package org.yourcompany.yourproject;
+
+public class Operadores {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- tipos-variaveis/src/main/java/org/yourcompany/yourproject/TiposVariaveis.java
+++ tipos-variaveis/src/main/java/org/yourcompany/yourproject/TiposVariaveis.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.yourcompany.yourproject;
 
-/**
- *
- * @author nortoncunha
- */
 public class TiposVariaveis {
 
     public static void main(String[] args) {
